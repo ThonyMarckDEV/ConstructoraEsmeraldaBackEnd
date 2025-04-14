10e26e64e1fc93226aad0a4e8c57330262f6f679
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-04-14 01:24:30 -0500

    CORRECTED

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,8 +19,7 @@ class CorsMiddleware
             return $next($request)
                 ->header('Access-Control-Allow-Origin', $origin)
                 ->header('Access-Control-Allow-Methods', '*')
-                // ->header('Access-Control-Allow-Headers', '*')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+                ->header('Access-Control-Allow-Headers', '*')
                 ->header('Access-Control-Allow-Credentials', 'false');
         }
 
