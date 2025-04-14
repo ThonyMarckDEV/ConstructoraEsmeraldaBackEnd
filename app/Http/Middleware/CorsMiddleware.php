af696c441b2ed652022f1c991b270d345fb12733
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-04-14 00:51:19 -0500

    CORRECTED MIDDLEWARE

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,10 +8,21 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', '*')
-            ->header('Access-Control-Allow-Credentials', 'false');
+        $allowedOrigins = [
+            'https://constructora-esmeralda-front-end.vercel.app',
+            'http://localhost:3000'
+        ];
+
+        $origin = $request->headers->get('Origin');
+
+        if (in_array($origin, $allowedOrigins)) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $origin)
+                ->header('Access-Control-Allow-Methods', '*')
+                ->header('Access-Control-Allow-Headers', '*')
+                ->header('Access-Control-Allow-Credentials', 'true');
+        }
+
+        return $next($request);
     }
 }
